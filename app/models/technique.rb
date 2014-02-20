dbbaf039f06a028855cb530531d587ce333a3a67
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 13:21:04 +0100

    relate techniques to users

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,6 +1,7 @@
 class Technique < ActiveRecord::Base
   belongs_to :belt
   belongs_to :category
+  belongs_to :user
 
   validates :name, presence: true,
                    uniqueness: true
--- spec/models/technique_spec.rb
+++ spec/models/technique_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Technique do
   it { should belong_to :belt }
   it { should belong_to :category }
+  it { should belong_to :user }
 
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
