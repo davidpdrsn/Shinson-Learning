a68ac53008af7d6edeac7e9ce3b7bcc3d89e8933
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 10:29:53 +0100

    create belt table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219171043) do
+ActiveRecord::Schema.define(version: 20140220092857) do
+
+  create_table "belts", force: true do |t|
+    t.string "color"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
