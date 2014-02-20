74ddf451e4ae89202b1573ada6c54445c30acd24
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 17:26:42 +0100

    add integration spec for creating in valid technique

--- spec/features/managing_techniques_spec.rb
+++ spec/features/managing_techniques_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 feature 'creating techniques' do
-  scenario 'a logged in user creates a technique' do
+  before do
     create(:belt, color: "White", degree: "Mu kup")
     create(:category, name: "Jokgi sul")
+  end
 
+  scenario 'a user creates a technique' do
     sign_up
     click_link "New technique"
     fill_in "Name", with: "Ap Chagi"
@@ -14,5 +16,16 @@ feature 'creating techniques' do
     click_button "Create Technique"
 
     expect(page).to have_content "Technique saved"
+  end
+
+  scenario 'a user creates an invalid technique' do
+    sign_up
+    click_link "New technique"
+    fill_in "Description", with: "Front kick"
+    select "White (mu kup)", from: "Belt"
+    select "Jokgi sul", from: "Category"
+    click_button "Create Technique"
+
+    expect(page).to have_content "Technique not valid"
   end
 end
