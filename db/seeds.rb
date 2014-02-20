ed6f5aabb1db473a59377928717907fec76b14bc
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 11:46:04 +0100

    update seeds file to create categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,38 @@ def create_belts
   end
 
   puts "There are now #{Belt.count} belts"
+  puts "\n===============\n\n"
+end
+
+def create_categories
+  Category.destroy_all
+
+  [
+    "sugi sul",
+    "jokgi sul",
+    "hyong",
+    "su",
+    "yaksok daeryon",
+    "yayo daeryon",
+    "gigu sul",
+    "yuldong undong",
+    "dan jeon hohupbob",
+    "nakbop",
+    "vocabulary",
+    "pressure point",
+    "jahse",
+    "kyokpa",
+    "theory",
+    "miscellaneous"
+  ].map(&:titleize).each do |name|
+    if Category.create(name: name)
+      puts "Created category #{name}"
+    end
+  end
+
+  puts "There are now #{Category.count} categories"
+  puts "\n===============\n\n"
 end
 
 create_belts
+create_categories
