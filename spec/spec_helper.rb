cad6120a49da35daa7fca1371016e0360da42cb7
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 16:49:57 +0100

    include devise controller helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+
   config.use_transactional_fixtures = false
 
   config.infer_base_class_for_anonymous_controllers = false
