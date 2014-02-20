5147b7ae7d5110f293e2c339bdaaea573e05cae5
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 17:52:55 +0100

    add feature test for viewing techniques

--- spec/features/managing_techniques_spec.rb
+++ spec/features/managing_techniques_spec.rb
@@ -30,3 +30,23 @@ feature 'creating techniques' do
     expect(page).to have_content "Technique not valid"
   end
 end
+
+feature 'viewing techniques' do
+  before do
+    create(:belt, color: "White", degree: "Mu kup")
+    create(:category, name: "Jokgi sul")
+  end
+
+  scenario 'a user view his list of todos' do
+    sign_up
+    click_link "New technique"
+    fill_in "Name", with: "Ap chagi"
+    fill_in "Description", with: "Front kick"
+    select "White (mu kup)", from: "Belt"
+    select "Jokgi sul", from: "Category"
+    click_button "Create Technique"
+    click_link "My techniques"
+
+    expect(page).to have_content "Ap chagi"
+  end
+end
