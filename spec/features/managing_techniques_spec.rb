c7d0cad14cc1d880657453172b23370b496a8a51
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 18:04:33 +0100

    refactor creation of techniques

--- spec/features/managing_techniques_spec.rb
+++ spec/features/managing_techniques_spec.rb
@@ -1,50 +1,25 @@
 require 'spec_helper'
 
 feature 'creating techniques' do
-  before do
-    create(:belt, color: "White", degree: "Mu kup")
-    create(:category, name: "Jokgi sul")
-  end
-
   scenario 'a user creates a technique' do
     sign_up
-    click_link "New technique"
-    fill_in "Name", with: "Ap Chagi"
-    fill_in "Description", with: "Front kick"
-    select "White (mu kup)", from: "Belt"
-    select "Jokgi sul", from: "Category"
-    click_button "Create Technique"
+    create_technique
 
     expect(page).to have_content "Technique saved"
   end
 
   scenario 'a user creates an invalid technique' do
     sign_up
-    click_link "New technique"
-    fill_in "Name", with: ""
-    fill_in "Description", with: "Front kick"
-    select "White (mu kup)", from: "Belt"
-    select "Jokgi sul", from: "Category"
-    click_button "Create Technique"
+    create_technique("")
 
     expect(page).to have_content "Technique not valid"
   end
 end
 
 feature 'viewing techniques' do
-  before do
-    create(:belt, color: "White", degree: "Mu kup")
-    create(:category, name: "Jokgi sul")
-  end
-
   scenario 'a user view his list of todos' do
     sign_up
-    click_link "New technique"
-    fill_in "Name", with: "Ap chagi"
-    fill_in "Description", with: "Front kick"
-    select "White (mu kup)", from: "Belt"
-    select "Jokgi sul", from: "Category"
-    click_button "Create Technique"
+    create_technique
     click_link "My techniques"
 
     expect(page).to have_content "Ap chagi"
---
+++ spec/support/technique_helpers.rb
@@ -0,0 +1,22 @@
+def create_technique(name="Ap chagi",
+                     description="foo",
+                     belt="White (mu kup)",
+                     category="Jokgi sul")
+
+  unless Category.find_by(name: category)
+    create(:category, name: category)
+  end
+
+  unless Belt.all.map.any? { |b| b.pretty_format == belt }
+    match = belt.match(/(?<color>.*?) \((?<degree>.*?)\)/)
+    Belt.create(color: match[:color], degree: match[:degree])
+  end
+
+  visit root_path
+  click_link "New technique"
+  fill_in "Name", with: name
+  fill_in "Description", with: description
+  select belt, from: "Belt"
+  select category, from: "Category"
+  click_button "Create Technique"
+end
