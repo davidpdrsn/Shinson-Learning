e8ca5139f6b1797f0d7eccb634519abc4f7a0ef0
David Pedersen <david.pdrsn@gmail.com>
2014-02-20 17:52:42 +0100

    add techniques show view and tests

---
+++ spec/views/techniques/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "techniques/index" do
+  context "there are are techniques" do
+    it "shows a list of the techniques" do
+      technique = build_stubbed(:technique)
+      assign(:techniques, [technique])
+
+      with_rendered_page do |page|
+        expect(page).to have_content technique.name
+      end
+    end
+  end
+
+  context "when there are no techniques" do
+    it "shows that there are no techniques" do
+      assign(:techniques, [])
+
+      with_rendered_page do |page|
+        expect(page).to have_content "You have no techniques yet"
+      end
+    end
+  end
+end
