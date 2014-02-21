986ca21bcc64df5f1d45727416fa97987b42b779
David Pedersen <david.pdrsn@gmail.com>
2014-02-21 17:16:11 +0100

    remove some redundant contexts

--- spec/views/techniques/index.html.erb_spec.rb
+++ spec/views/techniques/index.html.erb_spec.rb
@@ -1,26 +1,22 @@
 require 'spec_helper'
 
 describe "techniques/index" do
-  context "there are are techniques" do
-    it "shows a list of the techniques" do
-      technique = build_stubbed(:technique)
-      techniques = {
-        "Jokgi Sul" => { "White (mu kup)" => [technique] }
-      }
-      assign(:techniques, techniques)
+  it "shows a list of the techniques" do
+    technique = build_stubbed(:technique)
+    techniques = {
+      "Jokgi Sul" => { "White (mu kup)" => [technique] }
+    }
+    assign(:techniques, techniques)
 
-      render
-      expect(rendered).to match /Jokgi Sul.*White \(mu kup\).*#{technique.name}/m
-    end
+    render
+    expect(rendered).to match /Jokgi Sul.*White \(mu kup\).*#{technique.name}/m
   end
 
-  context "when there are no techniques" do
-    it "shows that there are no techniques" do
-      assign(:techniques, [])
+  it "shows when there are no techniques" do
+    assign(:techniques, [])
 
-      with_rendered_page do |page|
-        expect(page).to have_content "You have no techniques yet"
-      end
+    with_rendered_page do |page|
+      expect(page).to have_content "You have no techniques yet"
     end
   end
 end
