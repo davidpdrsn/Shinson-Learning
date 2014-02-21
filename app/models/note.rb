d3e41c6335797762bb510b26972859cca58ce66f
David Pedersen <david.pdrsn@gmail.com>
2014-02-21 23:22:50 +0100

    update validations on note

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,6 @@ class Note < ActiveRecord::Base
   belongs_to :technique
 
   validates :text, presence: true
+  validates :user_id, presence: true
+  validates :technique_id, presence: true
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,4 +5,6 @@ describe Note do
   it { should belong_to :technique }
 
   it { should validate_presence_of :text }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :technique_id }
 end
