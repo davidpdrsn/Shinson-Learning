92aca9ef02061722c7bdebfc6047eb0c14d1485e
David Pedersen <david.pdrsn@gmail.com>
2014-02-21 17:23:03 +0100

    show number of notes if there are any

--- spec/views/techniques/index.html.erb_spec.rb
+++ spec/views/techniques/index.html.erb_spec.rb
@@ -19,4 +19,17 @@ describe "techniques/index" do
       expect(page).to have_content "You have no techniques yet"
     end
   end
+
+  it "shows the note count when a technique has notes" do
+    technique = build_stubbed(:technique)
+    technique.stub(:note_count).and_return(2)
+    techniques = {
+      "Jokgi Sul" => { "White (mu kup)" => [technique] }
+    }
+    assign(:techniques, techniques)
+
+    with_rendered_page do |page|
+      expect(page).to have_content "(2 notes)"
+    end
+  end
 end
