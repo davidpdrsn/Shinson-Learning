869db4afa5d08a2fdb46c9eeb55e8ac1abca2235
David Pedersen <david.pdrsn@gmail.com>
2014-02-21 13:18:13 +0100

    update seeds file to create a bunch of techniques

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 def create_belts
-  Belt.destroy_all
-
   [
     ['white', 'mu kup'],
     ['yellow', '9 kup'],
@@ -20,9 +18,7 @@ def create_belts
     color = belt.first.titleize
     degree = belt.fetch(1)
 
-    if Belt.create(color: color, degree: degree)
-      puts "Created #{color}, #{degree}"
-    end
+    Belt.create(color: color, degree: degree)
   end
 
   puts "There are now #{Belt.count} belts"
@@ -30,8 +26,6 @@ def create_belts
 end
 
 def create_categories
-  Category.destroy_all
-
   [
     "dan jeon hohupbob",
     "gigu sul",
@@ -50,14 +44,124 @@ def create_categories
     "yayo daeryon",
     "yuldong undong"
   ].map(&:titleize).each do |name|
-    if Category.create(name: name)
-      puts "Created category #{name}"
-    end
+    Category.create(name: name)
   end
 
   puts "There are now #{Category.count} categories"
   puts "\n===============\n\n"
 end
 
+def create_techniques
+  Technique.destroy_all
+
+  black  = Belt.where(degree: "1 dan").first
+  yellow = Belt.where(color: "Yellow").first
+  white  = Belt.where(color: "White").first
+  su     = Category.where(name: "Su").first
+  kicks  = Category.where(name: "Jokgi Sul").first
+  me     = User.where(email: "david.pdrsn@gmail.com").first
+
+  {
+    "arrero" => [
+      "Blokering højt, skridt frem, albue lås",
+      "Skulderkast",
+      "Ud på siden, kniv i lår, yok sudo på hals",
+      "Yop chagi til albue",
+      "Sudo på hals"
+    ],
+    "paroh" => [
+      "Gibon sul 4",
+      "Sujang bangosul, sudo på underarm, slag på hals, ben fejning, gong gong",
+      "Baby",
+      "Håndledslås, (spark i mave), flyver med ben, skulderlås",
+      "Flyver"
+    ],
+    "annuro" => [
+      "Dobbelt sudo, albue i hovedet, ben fejning",
+      "Albuelås, murub chagi, hoftekast",
+      "Jackie Chan",
+      "Slyng",
+      "Nervepunkt ved krageben, trykke i gulvet"
+    ],
+    "bagguro" => [
+      "Gibon sul 5",
+      "Sujang sangdan, yok sudo under arm,  halslås klæbe",
+      "Jung jie maedup kwon til ribben",
+      "Albuelås over brystet",
+      "Træk ved skuldre skub i knæhase"
+    ]
+  }.each do |group, techniques|
+    techniques.each.with_index do |description, n|
+      Technique.create(
+        name: "Kal makki #{group} ##{n+1}",
+        description: description,
+        belt: black,
+        category: su,
+        user: me
+      )
+    end
+  end
+
+  [
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know...",
+    "You know..."
+  ].each.with_index do |description, n|
+    Technique.create(
+      name: "Gibon Sul ##{n+1}",
+      description: description,
+      belt: yellow,
+      category: su,
+      user: me
+    )
+  end
+
+  [
+    "Ap chagi",
+    "Dollyo chagi",
+    "Murub chagi",
+    "Andari chagi",
+    "Ap chaoligi"
+  ].each do |kick|
+    Technique.create(
+      name: kick,
+      description: "You know...",
+      belt: white,
+      category: kicks,
+      user: me
+    )
+  end
+
+  [
+    "Yop chagi",
+    "Dwidgumchi dollyo chagi",
+    "Dwidgumchi chaoligi",
+    "Balbadak dollyo chagi",
+    "Dwid chagi",
+    "Dwidgumchi dwid chaoligi"
+  ].each do |kick|
+    Technique.create(
+      name: kick,
+      description: "You know...",
+      belt: yellow,
+      category: kicks,
+      user: me
+    )
+  end
+end
+
 create_belts
 create_categories
+create_techniques
