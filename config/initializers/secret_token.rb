cd4a5a3fe7db640c2692bf71f168381d35018083
David Pedersen <david.pdrsn@gmail.com>
2014-02-23 20:01:36 +0100

    update secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ShinsonLearning::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
+ShinsonLearning::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"] || 'secret'
