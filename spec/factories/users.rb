53f21a9033059b89a83ab85dfeb4d195b773bb72
David Pedersen <david.pdrsn@gmail.com>
2014-02-24 15:28:28 +0100

    add first name and last name to user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,8 @@ FactoryGirl.define do
     sequence :email do |n|
       "person#{n}@example.com"
     end
+    first_name "bob"
+    last_name "johnson"
     password "passwordlol"
     password_confirmation "passwordlol"
   end
