1f7dd615f442a3f1caf4f05b92cb7a15363e4953
David Pedersen <david.pdrsn@gmail.com>
2014-02-25 15:33:15 +0100

    add script for populating the database with a bunch of test data

---
+++ scripts/add_test_data.rb
@@ -0,0 +1,27 @@
+include FactoryGirl::Syntax::Methods
+
+Technique.destroy_all
+User.destroy_all
+
+create(:user, email: "david.pdrsn@gmail.com",
+       password: "passwordlol",
+       password_confirmation: "passwordlol")
+
+100.times do
+  user = create(:user)
+  puts "Made user: #{user.id}"
+end
+
+1_000.times do |n|
+  user = User.all.sample
+  technique = Technique.new(
+    name: "technique ##{n}",
+    description: "descript for technique ##{n}",
+    belt: Belt.all.sample,
+    category: Category.all.sample,
+    user: user
+  )
+  if technique.save
+    p "Created technique ##{n} for user ##{user.id}"
+  end
+end
