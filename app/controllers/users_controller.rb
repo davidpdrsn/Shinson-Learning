421d86c6c84a87ffac6fe0ed4ffa44178d965a3b
David Pedersen <david.pdrsn@gmail.com>
2014-02-27 22:40:17 +0100

    use devise to edit and update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,7 @@
 class UsersController < ApplicationController
-  before_action :get_user, only: [:edit, :show, :update, :destroy]
-  before_action :enfore_user!, only: [:edit, :update, :destroy]
+  before_action :get_user, only: [:show]
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    @user.update_attributes(user_params)
-    redirect_to @user, notice: "Updated"
-  end
-
-  def destroy
-    @user.destroy
-    redirect_to root_path, notice: "Account deleted"
   end
 
   private
@@ -26,11 +12,5 @@ class UsersController < ApplicationController
 
   def get_user
     @user = User.find(params[:id])
-  end
-
-  def enfore_user!
-    unless current_user == @user
-      redirect_to @user, alert: "You can only edit your own account"
-    end
   end
 end
