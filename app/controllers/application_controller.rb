7fb54b26daa380d9490e7c674759caf004eab972
David Pedersen <david.pdrsn@gmail.com>
2014-02-27 18:55:32 +0100

    add user first and last name options and update feature specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :first_name, :last_name)
+    end
+  end
 
   def get_technique
     @technique = current_user.techniques.find(params[technique_param_key])
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 feature 'authentication' do
+  let(:user) { build :user, first_name: "Bob", last_name: "Anderson" }
+
   scenario 'a guest arrives at the home page' do
     visit root_path
 
@@ -13,10 +15,17 @@ feature 'authentication' do
     expect(page).to have_content "Welcome"
   end
 
+  scenario 'a user signs up and gives their name' do
+    sign_up user
+
+    expect(page).to have_content "Welcome"
+    expect(page).to have_content "Bob Anderson"
+  end
+
   scenario 'a user signs in' do
-    sign_up
+    sign_up user
     logout
-    sign_in
+    sign_in user
 
     expect(page).to have_content "Log out"
   end
