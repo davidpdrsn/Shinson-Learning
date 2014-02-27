8fcef42b68d58c99235c1f139c97853a70e776e1
David Pedersen <david.pdrsn@gmail.com>
2014-02-27 23:26:01 +0100

    remove dead code

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,12 +5,4 @@ describe Category do
   it { should validate_uniqueness_of :name }
 
   it { should have_many :techniques }
-
-  describe "#html_class" do
-    it "returns a string used for the html class" do
-      category = create(:category, name: "Jokgi sul")
-
-      expect(category.html_class).to eq "jokgi-sul"
-    end
-  end
 end
