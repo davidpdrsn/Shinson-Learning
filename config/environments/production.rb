8c619e4bb817e849875b98bd5d67d4f43cde72ae
David Pedersen <david.pdrsn@gmail.com>
2014-02-28 15:58:24 +0100

    configure email again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,20 @@ ShinsonLearning::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = {
+    host: => 'whatever-heroku-gives-you.herokuapp.com'
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 25,
+    domain: "heroku.com", 
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
+  }
 end
