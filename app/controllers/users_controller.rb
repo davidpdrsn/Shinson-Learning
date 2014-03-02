c333dbaf634ede5de985f341a5af0a3c30afd84d
David Pedersen <david.pdrsn@gmail.com>
2014-03-02 14:37:16 +0100

    remove dead code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,6 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit :first_name, :last_name, :email
-  end
-
   def get_user
     @user = User.find(params[:id])
   end
