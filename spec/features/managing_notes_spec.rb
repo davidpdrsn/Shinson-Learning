1ff23c43d7b9b5c9a3abf3c068b222004b0a395a
David Pedersen <david.pdrsn@gmail.com>
2014-03-07 14:31:48 +0100

    add smoke test around creating notes

--- spec/features/managing_notes_spec.rb
+++ spec/features/managing_notes_spec.rb
@@ -9,6 +9,17 @@ feature "adding a note to a technique" do
 
     expect(page).to have_content "Text of the new note"
   end
+
+  scenario "a user creates an invalid note, then fixes it" do
+    sign_up
+    create_technique
+    fill_in "Note text", with: ""
+    click_button "Create Note"
+    fill_in "Note text", with: "Text of the note"
+    click_button "Create Note"
+
+    expect(page).to have_content "Text of the note"
+  end
 end
 
 feature "editing a note" do
