76387143f988cb65c808cebe427498a721eef9a7
David Pedersen <david.pdrsn@gmail.com>
2014-03-07 15:29:45 +0100

    further cleaning of the techniques index view

--- app/presenters/technique_presenter.rb
+++ app/presenters/technique_presenter.rb
@@ -11,4 +11,20 @@ class TechniquePresenter < Presenter
   def belt_pretty_print
     Rails.cache.fetch([technique, technique.belt]) { technique.belt.pretty_print }
   end
+
+  # TODO: test this
+  def link
+    h.link_to name, technique, class: "technique__link"
+  end
+
+  # TODO: test this
+  def human_note_count
+    h.content_tag :span, "(#{note_count})", class: "note-count"
+  end
+
+  private
+
+  def note_count
+    h.pluralize technique.notes_count, "note"
+  end
 end
