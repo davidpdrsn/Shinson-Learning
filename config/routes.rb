0a0e19629d98d81a81971ddcca8465fd654c58fc
David Pedersen <david.pdrsn@gmail.com>
2014-03-31 10:42:03 +0200

    no parens, thank you

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ShinsonLearning::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  ActiveAdmin.routes self
 
   devise_for :users, controllers: { registrations: "registrations" }
 
