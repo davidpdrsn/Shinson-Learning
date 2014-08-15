45b6e57339ec2bae4aeaf9d52c8c344c36d23722
David Pedersen <david.pdrsn@gmail.com>
2014-08-15 11:45:54 +0200

    Small improvement to spec file

--- spec/cachers/study_cache_spec.rb
+++ spec/cachers/study_cache_spec.rb
@@ -1,13 +1,15 @@
 require "spec_helper"
 
 describe StudyCache do
-  it "caches average score", caching: true do
-    study = create :study, techniques_count: 10
-    study_cache = StudyCache.new(study)
-    create :score, study: study, correct_answers: 9, user: study.user
+  describe "#average_score" do
+    it "caches average score", caching: true do
+      study = create :study, techniques_count: 10
+      study_cache = StudyCache.new(study)
+      create :score, study: study, correct_answers: 9, user: study.user
 
-    expect do
-      create :score, study: study, correct_answers: 5, user: study.user
-    end.to change { study_cache.average_score }
+      expect do
+        create :score, study: study, correct_answers: 5, user: study.user
+      end.to change { study_cache.average_score }
+    end
   end
 end
