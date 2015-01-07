3bed4382eb03f874572b5526729d2c1b918eb5a1
David Pedersen <david.pdrsn@gmail.com>
2015-01-07 12:32:26 +0100

    Update some broken migrations

--- db/migrate/20140225141442_add_counter_cache_to_techniques_for_notes.rb
+++ db/migrate/20140225141442_add_counter_cache_to_techniques_for_notes.rb
@@ -1,10 +1,6 @@
 class AddCounterCacheToTechniquesForNotes < ActiveRecord::Migration
   def up
     add_column :techniques, :notes_count, :integer, default: 0, null: false
-
-    Technique.find_each(select: 'id') do |result|
-      Technique.reset_counters(result.id, :notes)
-    end
   end
 
   def down
